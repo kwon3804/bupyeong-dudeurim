6748a5fc1443a41beb0cf485a0f45a3fea683d56
kwon3804 <gurwn7878@naver.com>
2024-04-19 11:27:31 +0900

    fix file

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1---",
+      "name": "[1안] 공연장 및 회의실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8347628184047018,
+          "pitch": 0.28172236473489853,
+          "rotation": 5.497787143782138,
+          "target": "1-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1-",
+      "name": "[1안] 연습실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.708718550909869,
+          "pitch": 0.4083588767745159,
+          "rotation": 0.7853981633974483,
+          "target": "0-1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2---",
+      "name": "[2안] 공연장 및 회의실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8990123356892603,
+          "pitch": 0.2976369893519486,
+          "rotation": 5.497787143782138,
+          "target": "3-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2-",
+      "name": "[2안] 연습실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.014005765152312,
+          "pitch": 0.37227155860684746,
+          "rotation": 0.7853981633974483,
+          "target": "2-2---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "bupyeong-dudeurim",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
